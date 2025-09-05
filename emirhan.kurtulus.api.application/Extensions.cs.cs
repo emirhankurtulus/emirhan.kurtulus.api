4e6cab2fe417c613eefa14560f8955914f457a1a
Emirhan Kurtuluş <emirhn.kurtulus@functionair.com>
2025-09-06 00:04:31 +0300

    Project Files Created and MediatR Added

---
+++ emirhan.kurtulus.api.application/Extensions.cs.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace emirhan.kurtulus.api.application;
+
+public static class Extensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        return services;
+    }
+}
---
+++ emirhan.kurtulus.api.infrastructure/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace emirhan.kurtulus.api.infrastructure;
+
+public static class Extensions
+{
+    public static IServiceCollection AddInfrastructureServices(
+        IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        return services;
+    }
+}
